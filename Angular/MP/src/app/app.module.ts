fc5d47021f0f684a6cacde0adb00e74468a8ec50
ALICE SOARES <alicesoaresn@gmail.com>
2019-06-28 05:36:15 -0300

    30%project

--- Angular/MP/src/app/app.module.ts
+++ Angular/MP/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
